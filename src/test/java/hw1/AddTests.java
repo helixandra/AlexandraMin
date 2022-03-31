d5518446de0cf86353f2320d678013b1f3f482ee
Alexandra <alexa.min31@gmail.com>
2022-04-01 01:14:37 +0300

    corrected

--- src/test/java/hw1/AddTests.java
+++ src/test/java/hw1/AddTests.java
@@ -1,33 +1,31 @@
 package hw1;
 
-import com.epam.tat.module4.Calculator;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
-public class AddTests {
-    static final double EPS = 1e-7;
+public class AddTests extends BazeTestClass {
+
     @DataProvider
     public static Object[][] correctDoubleData() {
-        return new Object[][] {{1.0,2.5,3.5}, {8.7,9.225,17.925}, {1000.0, 105.0, 1105.0}};
+        return new Object[][]{{1.0, 2.5, 3.5}, {8.7, 9.225, 17.925}, {1000.0, 105.0, 1105.0}};
     }
+
     @DataProvider
     public static Object[][] correctLongData() {
-        return new Object[][] {{8569,274198,282767}, {159874,123698,283572}, {4, 3, 7}};
+        return new Object[][]{{8569, 274198, 282767}, {159874, 123698, 283572}, {4, 3, 7}};
     }
 
     @Test(dataProvider = "correctDoubleData")
-    public void AddDoubleTest (double num1, double num2, double expectedResult) {
-        Calculator calculator = new Calculator();
-        double actual = calculator.sum(num1,num2);
+    public void addDoubleTest(double num1, double num2, double expectedResult) {
+        double actual = calculator.sum(num1, num2);
         assertEquals(actual, expectedResult, EPS);
     }
 
     @Test(dataProvider = "correctLongData")
-    public void AddLongTest (long num1, long num2, long expectedResult) {
-        Calculator calculator = new Calculator();
-        long actual = calculator.sum(num1,num2);
+    public void addLongTest(long num1, long num2, long expectedResult) {
+        long actual = calculator.sum(num1, num2);
         assertEquals(actual, expectedResult);
     }
 }
--- src/test/java/hw1/DivideTests.java
+++ src/test/java/hw1/DivideTests.java
@@ -1,48 +1,36 @@
 package hw1;
 
-import com.epam.tat.module4.Calculator;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
-public class DivideTests {
-    static final double EPS = 1e-7;
-    private Calculator calculator;
-
-    @BeforeTest
-    public void doBeforeTest(){
-        calculator = new Calculator();
-    }
+public class DivideTests extends BazeTestClass {
 
     @DataProvider
     public static Object[][] correctDoubleData() {
-        return new Object[][] {{660.62,97.15,6.8}, {1840.16,164.3,11.2}, {35.7, 4.2, 8.5}};
+        return new Object[][]{{660.62, 97.15, 6.8}, {1840.16, 164.3, 11.2}, {35.7, 4.2, 8.5}};
     }
 
     @DataProvider
     public static Object[][] correctLongData() {
-        return new Object[][] {{67184,988,68}, {121726,22,5533}, {2418, 31, 78}};
+        return new Object[][]{{67184, 988, 68}, {121726, 22, 5533}, {2418, 31, 78}};
     }
 
-    @Test(expectedExceptions = { NumberFormatException.class } )
-    public void divisionByZeroExceptionTest() throws Exception {
+    @Test(expectedExceptions = {NumberFormatException.class})
+    public void divisionByZeroExceptionTest() {
         calculator.div(44, 0);
     }
 
     @Test(dataProvider = "correctLongData")
-    public void DivideLongTest (long num1, long num2, long expectedResult) {
-        Calculator calculator = new Calculator();
-        long actual = calculator.div(num1,num2);
+    public void divideLongTest(long num1, long num2, long expectedResult) {
+        long actual = calculator.div(num1, num2);
         assertEquals(actual, expectedResult);
     }
 
     @Test(dataProvider = "correctDoubleData")
-    public void DivideDoubleTest (double num1, double num2, double expectedResult) {
-        Calculator calculator = new Calculator();
-        double actual = calculator.div(num1,num2);
+    public void divideDoubleTest(double num1, double num2, double expectedResult) {
+        double actual = calculator.div(num1, num2);
         assertEquals(actual, expectedResult, EPS);
     }
-
 }
